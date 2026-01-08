46b94c636bd2755b4367804b66763a2c89f3d031
ahmedrachid04 <ahmedrachid0409@gmail.com>
2026-01-09 00:16:00 +0100

    added static dir

--- projet/settings.py
+++ projet/settings.py
@@ -123,9 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 import os
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
